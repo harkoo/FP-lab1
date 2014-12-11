01a98b15807fa190b1072047ae6d264756d8c121
Oleg Kharko <kharko1993@gmail.com>
2014-12-11 19:40:15 +0300

    distance functions

--- src/cluster/core.clj
+++ src/cluster/core.clj
@@ -1,7 +1,28 @@
 (ns cluster.core
   (:gen-class))
 
+(defn distance-hamming-func
+  [pos1 pos2]
+  (apply +
+    (map
+      (fn [a b] (if (= a b) 0 1))
+      pos1
+      pos2)))
+
+(defn distance-func-euclidean
+  [pos1 pos2]
+  (Math/sqrt
+    (apply +
+      (map
+        (fn [a b] (Math/pow (- a b) 2))
+        pos1
+        pos2))))
+
+(defn choose-distance-func
+  [distance-name]
+  (case distance-name
+    "hamming" distance-hamming-func
+    "euclidean" distance-func-euclidean))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [file distance-name])
--- test/cluster/core_test.clj
+++ test/cluster/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [cluster.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-distance-func-euclidean
+  (is (=
+    (distance-func-euclidean [11 7] [8 3])
+    5.0)))
+
+(deftest test-distance-hamming-func
+  (is (=
+    (distance-hamming-func [12 21 43 55 23] [12 25 43 54 23])
+    2)))
