152a7413eb284ef19ae3927e7d23528776dbf8e7
Oleg Kharko <kharko1993@gmail.com>
2014-12-13 17:00:22 +0300

    add test on potentials calculation

--- test/cluster/core_test.clj
+++ test/cluster/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [cluster.core :refer :all]))
 
+(deftest test-initialize-potentials
+  (is (=
+    (initialize-potentials '([12.32 4.23 6.42] [11.41 5.12 2.46]) distance-func-euclidean)
+    '({:pos [12.32 4.23 6.42], :pot 1.069800948794656} {:pos [11.41 5.12 2.46], :pot 1.069800948794656}))))
+
 (deftest test-distance-func-hamming
   (is (=
     (distance-func-hamming [12 21 43 55 23] [12 25 43 54 23])
